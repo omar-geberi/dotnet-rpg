0dea710df3da275fa571949fe4e559b6882dbbff
Admin - Omar Omar AL JUBAIRI <AdminOE@unops.org>
2020-12-05 00:05:37 +0200

    New Controller & GET a New Character

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController: ControllerBase
+    {
+        private static List<Character> characters= new List<Character>{
+            new Character()
+            //new Character{Name="Sam"}
+        };
+
+        public IActionResult Get()
+        {
+            return Ok(characters);
+        }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,16 @@
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int Hitpoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intellegence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+
+        
+
+    }
+}
